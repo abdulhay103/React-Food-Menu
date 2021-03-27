437ed6cb3d71390424c128dcf49a0c17711fc498
Abdul Hay <mdabdulhay95@gmail.com>
2021-03-28 01:11:12 +0600

    Food Menu Matcher Is Done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
-function App(items) {
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+// console.log(allCategories);
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+  // console.log(categories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <main>
       <section className="menu section">
@@ -11,8 +26,8 @@ function App(items) {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
-        <Menu />
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu menuItems={menuItems} />
       </section>
     </main>
   );
